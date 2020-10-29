c360d85aec0abacca18820fd1e4938883da797ec
AlexDoss42 <alex.doss42@gmail.com>
2020-10-29 17:03:44 -0600

    start to mess around with building a message

--- src/components/EmojiPicker.js
+++ src/components/EmojiPicker.js
@@ -4,9 +4,11 @@ import Picker from 'emoji-picker-react';
 const EmojiPicker = () => {
   const [chosenEmoji, setChosenEmoji] = useState(null);
   const [showPicker, toggleShowPicker] = useState(false);
+  const [msgText, setMsgText] = useState('');
 
   const onEmojiClick = (event, emojiObject) => {
     setChosenEmoji(emojiObject);
+    setMsgText(msgText + chosenEmoji.emoji);
   };
 
   const showPickerHandler = () => {
@@ -16,7 +18,7 @@ const EmojiPicker = () => {
   return (
     <div>
       {chosenEmoji ? (
-        <span>You chose: {chosenEmoji.emoji}</span>
+        <span>You chose: {msgText}</span>
       ) : (
         <span>No emoji Chosen</span>
       )}
